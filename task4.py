cbe0d4d9c274b7e320adda38774bbac01203a1ea
qqreyn <duch.jan.8@gmail.com>
2026-02-17 17:00:21 +0100

    Add comments for input conversion and output
    Added comments to clarify input conversion and output.

--- task4.py
+++ task4.py
@@ -1,8 +1,8 @@
 num_float = input("Enter float number:")
 num_int = input("Enter int number:")
-
+#Converts inputs to float (1.0) and int (1)
 x = float(num_float)
 y = int(num_int)
-
+#Shows the difference
 print(x)
 print(y)
