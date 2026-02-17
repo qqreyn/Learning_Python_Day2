7c5faf4bdb1b8868f4204508a95cdc94c7e1b85a
qqreyn <duch.jan.8@gmail.com>
2026-02-17 17:00:46 +0100

    Enhance task5.py with comments for clarity
    Added comments to clarify the purpose of the input and exception handling.

--- task5.py
+++ task5.py
@@ -1,8 +1,11 @@
 name = input("Enter your name:")
 
+#Try to convert string into an int number
+#Except prints error if input cannot be converted into int number
 try:
     x = int(name)
     print(x)
 except:
     print("ERROR: Input is not a int number!")
+
 
